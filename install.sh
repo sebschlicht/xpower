9a06c4350c59daebb784cd5fab125721a403a236
Sebastian Schlicht <sebastian@jablab.de>
2016-11-18 09:48:59 +0100

    installation script added (unpackaged version only)

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+print_error_exit () {
+  local msg="$1"
+  local code=1
+  if [ ! -z "$2" ]; then
+    local code="$2"
+  fi
+  echo "[ERROR] $msg" >&2
+  exit "$code"
+}
+
+# copy xpower to unmanaged script directory and set permissions
+f_script_src='xpower.sh'
+f_script_dest='/usr/local/bin/xpower'
+sudo cp "$f_script_src" "$f_script_dest" && sudo chmod +x "$f_script_dest"
+if [ "$?" != '0' ]; then
+  print_error_exit 'Failed to copy script or to set its permissions!'
+fi
+
+# copy udev rule into configuration folder and set permissions
+fname_rule='80-xpower_power-mode.rules'
+f rule_src="resources/$fname_rule"
+f_rule_dest="/etc/udev/rules.d/$fname_rule"
+sudo cp "$f_rule_src" "$f_rule_dest" && sudo chmod 311 "$f_rule_dest"
+if [ "$?" != '0' ]; then
+  print_error_exit 'Failed to copy udev rule or to set its permissions!'
+fi
+
+# copy systemd service config into configuration folder and enable it
+fname_systemd='xpower.service'
+f_systemd_src="resources/$fname_systemd"
+f_systemd_dest="/etc/systemd/system/$fname_systemd"
+sudo cp "$f_systemd_src" "$f_systemd_dest" && sudo systemctl enable "$fname_systemd"
+if [ "$?" != '0' ]; then
+  print_error_exit 'Failed to copy systemd config or to enable it!'
+fi
+
+# start systemd service to circument reboot
+sudo systemd restart "$fname_systemd"
