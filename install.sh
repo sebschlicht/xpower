43ecf6ab2d4c2e21a71df2814e73a465ab4ee08e
Sebastian Schlicht <sebastian@jablab.de>
2016-11-18 10:06:37 +0100

    fix bad udev file variable and systemd call

--- install.sh
+++ install.sh
@@ -20,21 +20,24 @@ fi
 
 # copy udev rule into configuration folder and set permissions
 fname_rule='80-xpower_power-mode.rules'
-f rule_src="resources/$fname_rule"
+f_rule_src="resources/$fname_rule"
 f_rule_dest="/etc/udev/rules.d/$fname_rule"
-sudo cp "$f_rule_src" "$f_rule_dest" && sudo chmod 311 "$f_rule_dest"
+sudo cp "$f_rule_src" "$f_rule_dest" && sudo chmod 644 "$f_rule_dest"
 if [ "$?" != '0' ]; then
   print_error_exit 'Failed to copy udev rule or to set its permissions!'
 fi
+# restart udev to circument reboot
+sudo service udev restart
 
 # copy systemd service config into configuration folder and enable it
 fname_systemd='xpower.service'
 f_systemd_src="resources/$fname_systemd"
 f_systemd_dest="/etc/systemd/system/$fname_systemd"
-sudo cp "$f_systemd_src" "$f_systemd_dest" && sudo systemctl enable "$fname_systemd"
+sudo cp "$f_systemd_src" "$f_systemd_dest"
+sudo systemctl enable "$fname_systemd"
 if [ "$?" != '0' ]; then
   print_error_exit 'Failed to copy systemd config or to enable it!'
 fi
-
 # start systemd service to circument reboot
-sudo systemd restart "$fname_systemd"
+sudo systemctl restart "$fname_systemd"
+
